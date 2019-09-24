e3051853bc80d8499c8883f1cd2e081f939cdf07
dgisolfi <daniel.gisolfi1@marist.edu>
2019-09-24 19:59:37 -0400

    Switched ID's to SERIAL and bytea to integer

--- database/war.sql
+++ database/war.sql
@@ -1,70 +1,65 @@
 -- File: War.sql
 -- Description: Create Sql Commands for War DB
 -- Date: 2019-09-20
--- Author: Dayna Eidle
+-- Author: Dayna Eidle 
+-- Updated By: Daniel Gisolfi
 
 -- create statements --
 
 --Reviewers--
 create table Reviewers(
-	reviewerId    integer not null,
+	reviewerId    SERIAL PRIMARY KEY,
 	emailAddress  char not null,
 	firstName     text not null,
 	lastName      text not null,
 	checkAdmin    text not null CHECK(checkAdmin = 'YES' or checkAdmin = 'NO'),
 	reputation    integer not null,
-  primary key (reviewerId)
 );
 
 --Sentences--
 create table Sentences(
-	sentenceId integer not null,
+	sentenceId SERIAL PRIMARY KEY,
 	userId     integer not null,
 	sentence   text not null,
-	rule1      bytea not null,
-	rule2      bytea not null,
-	rule3      bytea not null,
-	rule4      bytea not null,
-	rule5      bytea not null,
+	rule1      integer not null,
+	rule2      integer not null,
+	rule3      integer not null,
+	rule4      integer not null,
+	rule5      integer not null,
 	dateAdded  timestamp not null,
-  primary key(sentenceId)
 );
 
 --Reviews--
 create table Reviews(
-	reviewId      integer not null,
+	reviewId      SERIAL PRIMARY KEY,
 	reviewerId    integer not null references Reviewers(reviewerId),
 	sentenceId    integer not null references Sentences(sentenceId),
-	rule1         bytea not null,
-	rule2         bytea not null,
-	rule3         bytea not null,
-	rule4         bytea not null,
-	rule5         bytea not null,
+	rule1         integer not null,
+	rule2         integer not null,
+	rule3         integer not null,
+	rule4         integer not null,
+	rule5         integer not null,
 	dateAdded     timestamp not null,
-  primary key (reviewId)
 );
 
 --Dataset--
 create table Dataset(
 	sentenceId    integer not null references Sentences(sentenceId),
 	sentence      text not null,
-	rule1         bytea not null,
-	rule2         bytea not null,
-	rule3         bytea not null,
-	rule4         bytea not null,
-	rule5         bytea not null,
+	rule1         integer not null,
+	rule2         integer not null,
+	rule3         integer not null,
+	rule4         integer not null,
+	rule5         integer not null,
 	dateAdded     timestamp not null,
   primary key (sentenceId)
 );
 
 --Versions--
 create table Versions(
-	modelId     integer not null,
+	modelId     SERIAL PRIMARY KEY,
 	balAccuracy decimal not null,
 	ruleNum     integer not null,
 	loc         text not null,
 	dateAdded   timestamp not null,
-  primary key (modelId)
 );
-
-
