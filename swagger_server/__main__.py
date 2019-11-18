6fb1075435d28425e9788795fbf13a202b10096c
dgisolfi <daniel.gisolfi1@marist.edu>
2019-11-18 18:43:32 -0500

    Added cors for API...it was super easy im just bad at googling

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -2,19 +2,14 @@
 
 import connexion
 from swagger_server import encoder
+from flask_cors import CORS 
 
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
+    CORS(app.app)
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'WAR API'})
-
-    @app.after_request
-    def add_headers(response):
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-
     app.run(port=8080, debug=False)
-
 
 if __name__ == '__main__':
     main()
